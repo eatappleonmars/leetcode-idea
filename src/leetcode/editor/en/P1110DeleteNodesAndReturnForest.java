7085b076cc9c82e6bc22ed04ff3c0fc3dd91a8db
chen-fang <eatappleonmars@gmail.com>
2022-01-10 11:00:19 -0700

    Add 1110

---
+++ src/leetcode/editor/en/P1110DeleteNodesAndReturnForest.java
@@ -0,0 +1,106 @@
+//Given the root of a binary tree, each node in the tree has a distinct value. 
+//
+// After deleting all nodes with a value in to_delete, we are left with a 
+//forest (a disjoint union of trees). 
+//
+// Return the roots of the trees in the remaining forest. You may return the 
+//result in any order. 
+//
+// 
+// Example 1: 
+//
+// 
+//Input: root = [1,2,3,4,5,6,7], to_delete = [3,5]
+//Output: [[1,2,null,4],[6],[7]]
+// 
+//
+// Example 2: 
+//
+// 
+//Input: root = [1,2,4,null,3], to_delete = [3]
+//Output: [[1,2,4]]
+// 
+//
+// 
+// Constraints: 
+//
+// 
+// The number of nodes in the given tree is at most 1000. 
+// Each node has a distinct value between 1 and 1000. 
+// to_delete.length <= 1000 
+// to_delete contains distinct values between 1 and 1000. 
+// 
+// Related Topics Tree Depth-First Search Binary Tree 👍 2541 👎 73
+
+
+package leetcode.editor.en;
+
+// 2022-01-10 09:54:54
+
+import utils.TreeNode;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+public class P1110DeleteNodesAndReturnForest {
+    
+    //leetcode submit region begin(Prohibit modification and deletion)
+    /**
+     * Definition for a binary tree node.
+     * public class TreeNode {
+     *     int val;
+     *     TreeNode left;
+     *     TreeNode right;
+     *     TreeNode() {}
+     *     TreeNode(int val) { this.val = val; }
+     *     TreeNode(int val, TreeNode left, TreeNode right) {
+     *         this.val = val;
+     *         this.left = left;
+     *         this.right = right;
+     *     }
+     * }
+     */
+    class Solution {
+        public List<TreeNode> delNodes(TreeNode root, int[] to_delete) {
+            List<TreeNode> res = new ArrayList<>();
+            Set<Integer> toDelete = Arrays.stream(to_delete).boxed().collect(Collectors.toSet());
+            TreeNode node = delNodes(root, toDelete, res);
+            if (node != null) {
+                res.add(root);
+            }
+            return res;
+        }
+
+        private TreeNode delNodes(TreeNode root, Set<Integer> toDelete, List<TreeNode> res) {
+            if (root == null) {
+                return null;
+            }
+
+            root.left = delNodes(root.left, toDelete, res);
+            root.right = delNodes(root.right, toDelete, res);
+
+            if (toDelete.contains(root.val)) {
+                if (root.left != null) res.add(root.left);
+                if (root.right != null) res.add(root.right);
+                return null;
+            }
+
+            return root;
+        }
+    }
+    //leetcode submit region end(Prohibit modification and deletion)
+
+    
+    /**
+     * Test
+     */
+    public static void main(String[] args) {
+        Solution sol = new P1110DeleteNodesAndReturnForest().new Solution();
+        
+    } 
+}
