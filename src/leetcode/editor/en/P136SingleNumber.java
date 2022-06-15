d05fab91f04a5d7fd721848dc31cb9387f722e49
chen-fang <eatappleonmars@gmail.com>
2022-06-15 09:36:03 -0600

    Add 136

---
+++ src/leetcode/editor/en/P136SingleNumber.java
@@ -0,0 +1,56 @@
+//Given a non-empty array of integers nums, every element appears twice except 
+//for one. Find that single one. 
+//
+// You must implement a solution with a linear runtime complexity and use only 
+//constant extra space. 
+//
+// 
+// Example 1: 
+// Input: nums = [2,2,1]
+//Output: 1
+// Example 2: 
+// Input: nums = [4,1,2,1,2]
+//Output: 4
+// Example 3: 
+// Input: nums = [1]
+//Output: 1
+// 
+// 
+// Constraints: 
+//
+// 
+// 1 <= nums.length <= 3 * 10⁴ 
+// -3 * 10⁴ <= nums[i] <= 3 * 10⁴ 
+// Each element in the array appears twice except for one element which appears 
+//only once. 
+// 
+// Related Topics Array Bit Manipulation 👍 10091 👎 373
+
+
+package leetcode.editor.en;
+
+// 2022-06-15 09:27:10
+
+public class P136SingleNumber {
+    
+    //leetcode submit region begin(Prohibit modification and deletion)
+    class Solution {
+        public int singleNumber(int[] nums) {
+            int res = 0;
+            for (int n : nums) {
+                res ^= n;
+            }
+            return res;
+        }
+    }
+    //leetcode submit region end(Prohibit modification and deletion)
+
+    
+    /**
+     * Test
+     */
+    public static void main(String[] args) {
+        Solution sol = new P136SingleNumber().new Solution();
+        
+    } 
+}
