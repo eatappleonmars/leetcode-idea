368dcf885267090203024a943c11c93b6f322d2e
chen-fang <eatappleonmars@gmail.com>
2023-04-01 10:42:29 -0600

    Add 21

---
+++ src/leetcode/editor/en/P21MergeTwoSortedLists.java
@@ -0,0 +1,96 @@
+//You are given the heads of two sorted linked lists list1 and list2. 
+//
+// Merge the two lists in a one sorted list. The list should be made by 
+//splicing together the nodes of the first two lists. 
+//
+// Return the head of the merged linked list. 
+//
+// 
+// Example 1: 
+// 
+// 
+//Input: list1 = [1,2,4], list2 = [1,3,4]
+//Output: [1,1,2,3,4,4]
+// 
+//
+// Example 2: 
+//
+// 
+//Input: list1 = [], list2 = []
+//Output: []
+// 
+//
+// Example 3: 
+//
+// 
+//Input: list1 = [], list2 = [0]
+//Output: [0]
+// 
+//
+// 
+// Constraints: 
+//
+// 
+// The number of nodes in both lists is in the range [0, 50]. 
+// -100 <= Node.val <= 100 
+// Both list1 and list2 are sorted in non-decreasing order. 
+// 
+//
+// Related Topics Linked List Recursion 👍 17724 👎 1633
+
+
+package leetcode.editor.en;
+
+// 2023-04-01 10:32:33
+
+import utils.ListNode;
+
+public class P21MergeTwoSortedLists {
+    
+    //leetcode submit region begin(Prohibit modification and deletion)
+    /**
+     * Definition for singly-linked list.
+     * public class ListNode {
+     *     int val;
+     *     ListNode next;
+     *     ListNode() {}
+     *     ListNode(int val) { this.val = val; }
+     *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+     * }
+     */
+    class Solution {
+        public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+            ListNode head = new ListNode();
+            ListNode tail = head;
+            while (list1 != null && list2 != null) {
+                if (list1.val <= list2.val) {
+                    tail.next = list1;
+                    list1 = list1.next;
+                } else {
+                    tail.next = list2;
+                    list2 = list2.next;
+                }
+                tail = tail.next;
+            }
+            if (list1 != null) {
+                tail.next = list1;
+            }
+            if (list2 != null) {
+                tail.next = list2;
+            }
+            return head.next;
+        }
+
+
+    }
+    //leetcode submit region end(Prohibit modification and deletion)
+
+    
+    /**
+     * Test
+     */
+    public static void main(String[] args) {
+        Solution sol = new P21MergeTwoSortedLists().new Solution();
+        
+    } 
+}
