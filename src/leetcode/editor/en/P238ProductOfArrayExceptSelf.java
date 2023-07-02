72d6d8b546ae8954a6e554af75516f71b879a08b
chen-fang <eatappleonmars@gmail.com>
2023-07-02 16:05:02 -0700

    Update 238

--- src/leetcode/editor/en/P238ProductOfArrayExceptSelf.java
+++ src/leetcode/editor/en/P238ProductOfArrayExceptSelf.java
@@ -39,21 +39,29 @@ public class P238ProductOfArrayExceptSelf {
     
     //leetcode submit region begin(Prohibit modification and deletion)
     class Solution {
-        public int[] productExceptSelf(int[] nums) {
-            int len = nums.length;
-            int[] result = new int[len];
 
-            result[0] = 1;
-            for (int i = 1; i < len; i++) {
-                result[i] = result[i - 1] * nums[i - 1];
+        public int[] productExceptSelf(int[] nums) {
+            int n = nums.length;
+            int[] array1 = new int[n];
+            int[] array2 = new int[n];
+
+            array1[0] = 1;
+            for (int i = 1; i < n; i++) {
+                array1[i] = array1[i-1] * nums[i-1];
             }
-            int rtProduct = 1;
-            for (int i = len - 2; i >= 0; i--) {
-                rtProduct *= nums[i + 1];
-                result[i] *= rtProduct;
+
+            array2[n-1] = 1;
+            for (int i = n-2; i >= 0; i--) {
+                array2[i] = array2[i+1] * nums[i+1];
             }
-            return result;
+
+            int[] res = new int[n];
+            for (int i = 0; i < n; i++) {
+                res[i] = array1[i] * array2[i];
+            }
+            return res;
         }
+
     }
     //leetcode submit region end(Prohibit modification and deletion)
 
