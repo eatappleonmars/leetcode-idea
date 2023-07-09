376d9a48bdafe10588f79fa74f8e32f63b61c60a
chen-fang <eatappleonmars@gmail.com>
2023-07-09 14:18:48 -0600

    Add 242

---
+++ src/leetcode/editor/en/P242ValidAnagram.java
@@ -0,0 +1,66 @@
+//Given two strings s and t, return true if t is an anagram of s, and false 
+//otherwise. 
+//
+// An Anagram is a word or phrase formed by rearranging the letters of a 
+//different word or phrase, typically using all the original letters exactly once. 
+//
+// 
+// Example 1: 
+// Input: s = "anagram", t = "nagaram"
+//Output: true
+// 
+// Example 2: 
+// Input: s = "rat", t = "car"
+//Output: false
+// 
+// 
+// Constraints: 
+//
+// 
+// 1 <= s.length, t.length <= 5 * 10⁴ 
+// s and t consist of lowercase English letters. 
+// 
+//
+// 
+// Follow up: What if the inputs contain Unicode characters? How would you 
+//adapt your solution to such a case? 
+//
+// Related Topics Hash Table String Sorting 👍 9652 👎 307
+
+
+package leetcode.editor.en;
+
+// 2023-07-09 14:17:01
+
+public class P242ValidAnagram {
+    
+    //leetcode submit region begin(Prohibit modification and deletion)
+    class Solution {
+        public boolean isAnagram(String s, String t) {
+            if (s.length() != t.length()) {
+                return false;
+            }
+            int[] counts = new int[26];
+            for (char c : s.toCharArray()) {
+                counts[c - 'a']++;
+            }
+            for (char c : t.toCharArray()) {
+                if (counts[c - 'a'] == 0) {
+                    return false;
+                }
+                counts[c - 'a']--;
+            }
+            return true;
+        }
+    }
+    //leetcode submit region end(Prohibit modification and deletion)
+
+    
+    /**
+     * Test
+     */
+    public static void main(String[] args) {
+        Solution sol = new P242ValidAnagram().new Solution();
+        
+    } 
+}
